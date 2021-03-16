e590ad6c314ff4cc0558c7f197b3f9571fa384f6
girjesh42 <kumargirjesh2442@gmail.com>
2021-03-16 10:37:47 +0530

    second commit

---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC1GitBranch.Models
+{
+    public class Emp
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public string Edesig { get; set; }
+        public DateTime Edoj { get; set; }
+    }
+}
